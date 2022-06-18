3ce49d500bcc091d64623bc783757f92982c9db5
Dexter <dextermorgan9513@gmail.com>
2022-06-18 11:26:18 +0545

    insert

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,8 +1,8 @@
 module Comparable
   def compare(node)
-    if self.value > node.value
+    if node.value > value
       1
-    elsif self.value < node.value
+    elsif node.value < value
       -1
     else
       0
@@ -20,6 +20,10 @@ class Node
     @left_child = nil
     @right_child = nil
   end
+
+  def leaf?
+    left_child.nil? || right_child.nil?
+  end
 end
 
 class Tree
@@ -27,22 +31,49 @@ class Tree
   attr_accessor :array
 
   def initialize(array)
-     @root = build_tree(array.uniq.sort)
+    @root = build_tree(array.uniq.sort)
   end
 
-  def build_tree(array = @array)
+  def build_tree(array)
     return nil if array.empty?
 
     length = array.size
     mid_element = array[length / 2]
-    @root = Node.new(mid_element)
-    @root.left_child = Tree.new(array[0...length / 2])
-    @root.right_child = Tree.new(array[(length / 2 + 1)..-1])
-    @root
+    node = Node.new(mid_element)
+    node.left_child = build_tree(array[0...length / 2])
+    node.right_child = build_tree(array[(length / 2 + 1)..-1])
+    node
+  end
+
+  def insert(value)
+    new_node = Node.new(value)
+    cursor = @root
+    loop do
+      case cursor.compare(new_node)
+      when 1
+        if cursor.right_child.nil?
+          cursor.right_child = new_node
+          break
+        else 
+          cursor = cursor.right_child
+        end
+      when -1
+        if cursor.left_child.nil?
+          cursor.left_clild = new_node
+          break
+        else 
+          cursor = cursor.left_child
+        end
+      else
+        return 'Number already in the tree'
+      end
+
+    end
   end
 end
 
 tree = Tree.new([5, 6, 2, 4, 1, 9, 0, 8, 5, 6, 4, 3])
 require 'pry-byebug'
 binding.pry
+tree.insert(3.5)
 puts 'done'
